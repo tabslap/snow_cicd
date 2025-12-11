793a25b50491328ce63cf0223914e54372f94ae2
tabslap <tabishlap764@gmail.com>
2025-12-11 11:01:08 +0530

    added procedure and called it

--- ddl/employee_table.sql
+++ ddl/employee_table.sql
@@ -8,4 +8,11 @@ INSERT INTO SF.REPO.EMPLOYEE (ID, NAME, DEPARTMENT)
 VALUES
     (1099, 'Tabish', 'Delivery'),
     (1002, 'Sara', 'HR'),
-    (1003, 'Mike', 'Finance');
+    (1003, 'Mike', 'Finance'),
+    (1004, 'Riya', 'Sales'),
+    (1005, 'Arjun', 'Marketing'),
+    (1006, 'Isha', 'Tech Support'),
+    (1007, 'Rahul', 'Operations'),
+    (1008, 'Meera', 'Admin'),
+    (1009, 'Karan', 'IT'),
+    (1010, 'Zoya', 'Product');
